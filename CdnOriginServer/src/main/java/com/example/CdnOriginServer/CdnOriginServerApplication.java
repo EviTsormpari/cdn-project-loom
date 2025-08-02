3ddf6c0562108e4569cc897a7bc476c1dd40375c
EviTsormpari <evitsormpari16@gmail.com>
2025-08-02 12:28:45 +0300

    First Commit

---
+++ CdnOriginServer/src/main/java/com/example/CdnOriginServer/CdnOriginServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CdnOriginServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnOriginServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdnOriginServerApplication.class, args);
+	}
+
+}
---
+++ CdnOriginServer/src/test/java/com/example/CdnOriginServer/CdnOriginServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CdnOriginServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdnOriginServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
