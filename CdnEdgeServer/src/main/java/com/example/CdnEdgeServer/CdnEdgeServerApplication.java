222adbbf4d019211e365c05fbe959bc049e2fc2a
EviTsormpari <evitsormpari16@gmail.com>
2025-08-07 22:23:49 +0300

    Initial commit for Edge Server Project setup

---
+++ CdnEdgeServer/src/main/java/com/example/CdnEdgeServer/CdnEdgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CdnEdgeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnEdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdnEdgeServerApplication.class, args);
+	}
+
+}
