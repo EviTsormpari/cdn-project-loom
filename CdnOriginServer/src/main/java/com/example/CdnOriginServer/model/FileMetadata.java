b7829660f44cd44e2a6083ab54bcc0b8e3df22b5
EviTsormpari <evitsormpari16@gmail.com>
2025-09-23 00:41:42 +0300

    Add file cleanup initialize logic

--- CdnOriginServer/src/main/java/com/example/CdnOriginServer/model/FileMetadata.java
+++ CdnOriginServer/src/main/java/com/example/CdnOriginServer/model/FileMetadata.java
@@ -1,8 +1,6 @@
 package com.example.CdnOriginServer.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import org.springframework.data.domain.Persistable;
 
--- CdnOriginServer/src/main/java/com/example/CdnOriginServer/repository/OriginRepository.java
+++ CdnOriginServer/src/main/java/com/example/CdnOriginServer/repository/OriginRepository.java
@@ -4,8 +4,6 @@ import com.example.CdnOriginServer.model.FileMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.*;
-
 /* Το OriginRepository αξιοποιεί το Spring Data JPA για την παροχή βασικών CRUD λειτουργιών,
  ενώ επιτρέπει τον ορισμό επιπλέον μεθόδων αναζήτησης ή διαγραφής με βάση συγκεκριμένα πεδία (π.χ. filename).
  */
